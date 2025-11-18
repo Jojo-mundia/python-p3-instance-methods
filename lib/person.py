57e32d1102638f938a62db474594a55c71f9bd61
Joanne Kagio <kagiojoanne@gmail.com>
2025-11-18 23:34:18 +0300

    Initial commit

--- lib/person.py
+++ lib/person.py
@@ -2,6 +2,11 @@
 
 class Person:
     # Class body goes here
+  def talk(self):
+        print("Hello World!")
 
+    # Walk method
+  def walk(self):
+        print("The person is walking.")
     #Instance method definition
-    pass
+    
